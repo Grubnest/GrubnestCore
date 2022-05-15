98bbf372e650c4718e84fb504daf5fcffc66bff7
tamilpp25 <tamilpp25@gmail.com>
2022-05-15 22:46:42 +0530

    Feature: MySQL database connection pool using HikariCP

--- src/main/java/com/grubnest/game/core/GrubnestCorePlugin.java
+++ src/main/java/com/grubnest/game/core/GrubnestCorePlugin.java
@@ -1,15 +1,22 @@
 package com.grubnest.game.core;
 
+import com.grubnest.game.core.DatabaseHandler.MySQL;
+import com.grubnest.game.core.DatabaseHandler.MySQLData;
+import com.grubnest.game.core.DatabaseHandler.Utils.Disabler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GrubnestCorePlugin extends JavaPlugin {
-
+    private MySQL sql;
+    private static GrubnestCorePlugin instance;
     /**
      * Runs when plugin is enabled
      */
     @Override
     public void onEnable() {
-
+        instance = this;
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        sql = new MySQL(dataInitializer());
     }
 
     /**
@@ -17,6 +24,27 @@ public class GrubnestCorePlugin extends JavaPlugin {
      */
     @Override
     public void onDisable() {
+        Disabler.getInstance().disableAll();
+    }
 
+    private MySQLData dataInitializer(){
+        String host = getConfig().getString("Database.hostname");
+        String port = getConfig().getString("Database.port");
+        String database = getConfig().getString("Database.database");
+        String username = getConfig().getString("Database.username");
+        String password = getConfig().getString("Database.password");
+
+        int minimumConnections = getConfig().getInt("Database.minimumConnections");
+        int maximumConnections = getConfig().getInt("Database.maximumConnections");
+        long connectionTimeout = getConfig().getLong("Database.connectionTimeout");
+        return new MySQLData(host,username,password,port,database,minimumConnections,maximumConnections,connectionTimeout);
+    }
+
+    public MySQL getMySQL(){
+        return sql;
+    }
+
+    public static GrubnestCorePlugin getInstance(){
+        return instance;
     }
 }
