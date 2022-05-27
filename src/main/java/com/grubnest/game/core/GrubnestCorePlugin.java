849d909d993bf25ad112cde70821dcff8b16b652
Theeef <mcwyzards@gmail.com>
2022-05-26 23:11:39 -0400

    Revert "Hotfix sql spigot link"

---
+++ src/main/java/com/grubnest/game/core/GrubnestCorePlugin.java
@@ -0,0 +1,22 @@
+package com.grubnest.game.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GrubnestCorePlugin extends JavaPlugin {
+
+    /**
+     * Runs when plugin is enabled
+     */
+    @Override
+    public void onEnable() {
+
+    }
+
+    /**
+     * Runs when plugin is disabled
+     */
+    @Override
+    public void onDisable() {
+
+    }
+}
