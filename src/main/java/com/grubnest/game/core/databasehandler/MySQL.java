170f0e7cf76db564aab68d67522b06a641ae7bb9
Starky <mcwyzards@gmail.com>
2022-05-29 02:30:14 -0400

    Deleted util class, setup pom.xml for deployment

--- src/main/java/com/grubnest/game/core/databasehandler/MySQL.java
+++ src/main/java/com/grubnest/game/core/databasehandler/MySQL.java
@@ -4,6 +4,7 @@ import com.velocitypowered.api.proxy.Player;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
@@ -72,5 +73,30 @@ public class MySQL extends ConnectionPoolManager {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Gets a player's UUID from their last stored username
+     *
+     * @param username the player's username
+     * @return the player's uuid
+     */
+    public UUID getIdFromUsername(String username) {
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("""
+                    SELECT uuid
+                    FROM player
+                    WHERE username = ?
+                    """);
+            statement.setString(1, username);
+            ResultSet queryResults = statement.executeQuery();
+            statement.close();
+
+            return UUID.fromString(queryResults.getString(1));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        throw new NullPointerException("Could not retrive a UUID for a user with the name \"" + username + "\"");
     }
 }
