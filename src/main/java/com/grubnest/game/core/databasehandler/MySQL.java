9324a3a6333cb710da78d19f2ecd1113c6def813
Starky <mcwyzards@gmail.com>
2022-05-31 02:46:49 -0400

    Changed queries to try w/ resources, updated catch stacktrace

--- src/main/java/com/grubnest/game/core/databasehandler/MySQL.java
+++ src/main/java/com/grubnest/game/core/databasehandler/MySQL.java
@@ -15,7 +15,7 @@ import java.util.UUID;
  * make a new instance of this if you really know what you are doing!
  *
  * @author tamilpp25, Theeef
- * @version 1.1 at 5/29/2022
+ * @version 1.1 at 5/31/2022
  */
 public class MySQL extends ConnectionPoolManager {
     public MySQL(MySQLData data) {
@@ -33,17 +33,16 @@ public class MySQL extends ConnectionPoolManager {
      * Creates any tables required for the database. Runs at proxy server initialization
      */
     public void createTables() {
-        try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement("""
-                    CREATE TABLE IF NOT EXISTS `player` (
-                        uuid varchar(36) PRIMARY KEY,
-                        username varchar(16)
-                    )
-                    """);
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement("""
+                     CREATE TABLE IF NOT EXISTS `player` (
+                         uuid varchar(36) PRIMARY KEY,
+                         username varchar(16)
+                     )
+                     """)) {
             statement.executeUpdate();
-            statement.close();
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Couldn't create player table");
         }
     }
 
@@ -54,22 +53,21 @@ public class MySQL extends ConnectionPoolManager {
      * @param username the player's username
      */
     public void updatePlayerUsername(UUID id, String username) {
-        try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement("""
-                    INSERT INTO player
-                    	(uuid, username)
-                    VALUES
-                    	(?, ?)
-                    ON DUPLICATE KEY UPDATE
-                    	username = ?;
-                    	""");
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement("""
+                     INSERT INTO player
+                     	(uuid, username)
+                     VALUES
+                     	(?, ?)
+                     ON DUPLICATE KEY UPDATE
+                     	username = ?;
+                     	""")) {
             statement.setString(1, id.toString());
             statement.setString(2, username);
             statement.setString(3, username);
             statement.executeUpdate();
-            statement.close();
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Could not update username of player with UUID " + id.toString() + " and username " + username);
         }
     }
 
@@ -80,22 +78,19 @@ public class MySQL extends ConnectionPoolManager {
      * @return the player's uuid
      */
     public UUID getIdFromUsername(String username) {
-        try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement("""
-                    SELECT uuid
-                    FROM player
-                    WHERE username = ?
-                    """);
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement("""
+                     SELECT uuid
+                     FROM player
+                     WHERE username = ?
+                     """)) {
             statement.setString(1, username);
             ResultSet queryResults = statement.executeQuery();
-            statement.close();
 
             return UUID.fromString(queryResults.getString(1));
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Could not get UUID of player with username \"" + username + "\"");
         }
-
-        throw new NullPointerException("Could not retrieve a UUID for a user with the name \"" + username + "\"");
     }
 
     /**
@@ -117,9 +112,7 @@ public class MySQL extends ConnectionPoolManager {
 
             return queryResults.getString(1);
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Could not retrieve a username for a user with the uuid \"" + id.toString() + "\"");
         }
-
-        throw new NullPointerException("Could not retrieve a username for a user with the uuid \"" + id.toString() + "\"");
     }
 }
