f8f21ed893263c16cd0c090117a092e93b3e22fd
Starky <mcwyzards@gmail.com>
2022-06-02 01:12:34 -0400

    Static getUsernameFromID

--- src/main/java/com/grubnest/game/core/databasehandler/utils/DataUtils.java
+++ src/main/java/com/grubnest/game/core/databasehandler/utils/DataUtils.java
@@ -9,6 +9,7 @@ public class DataUtils {
     /**
      * Updates a players username stored in the database
      *
+     * @param mysql    the mysql instance
      * @param id       the player's uuid
      * @param username the player's username
      */
@@ -19,10 +20,22 @@ public class DataUtils {
     /**
      * Gets a player's UUID from their last stored username
      *
+     * @param mysql    the mysql instance
      * @param username the player's username
      * @return the player's uuid
      */
-    public UUID getIdFromUsername(MySQL mysql, String username) {
+    public UUID getIDFromUsername(MySQL mysql, String username) {
         return mysql.getIdFromUsername(username);
+    }
+
+    /**
+     * Get a players last stored username from their uuid
+     *
+     * @param mysql the mysql instance
+     * @param id    the player's uuid
+     * @return the player's username
+     */
+    public String getUsernameFromID(MySQL mysql, UUID id) {
+        return mysql.getUsernameFromID(id);
     }
 }
