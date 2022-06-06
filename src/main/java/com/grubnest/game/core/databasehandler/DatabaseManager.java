e262d3d1ca2df4f72ca34bedaa1074cfe1fe6169
Starky <mcwyzards@gmail.com>
2022-06-06 02:16:31 -0400

    Documentation

--- src/main/java/com/grubnest/game/core/databasehandler/DatabaseManager.java
+++ src/main/java/com/grubnest/game/core/databasehandler/DatabaseManager.java
@@ -4,6 +4,12 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Objects;
 
+/**
+ * Handles connection access for all other Grubnest Plugins
+ *
+ * @author Theeef
+ * @version 1.0 at 6/6/2022
+ */
 public class DatabaseManager {
 
     private static DatabaseManager instance;
@@ -13,14 +19,30 @@ public class DatabaseManager {
         this.mysql = new MySQL(MySQLData.dataInitializer());
     }
 
+    /**
+     * Gets the Singleton instance of the DatabaseManager
+     *
+     * @return the database manager
+     */
     public static DatabaseManager getInstance() {
         return Objects.requireNonNullElseGet(instance, () -> instance = new DatabaseManager());
     }
 
-    public static Connection getConnection() throws SQLException {
-        return getInstance().mysql.getConnection();
+    /**
+     * Gets a connection from the ConnectionPool
+     *
+     * @return a connection
+     * @throws SQLException
+     */
+    public Connection getConnection() throws SQLException {
+        return this.mysql.getConnection();
     }
 
+    /**
+     * Gets the MySQL connection
+     *
+     * @return mysql object
+     */
     public MySQL getMySQL() {
         return this.mysql;
     }
