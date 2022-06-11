d64df51b447ee24c12064c0ca6e53a90de23188f
Starky <mcwyzards@gmail.com>
2022-06-11 12:45:38 -0400

    Removed mysql parameters from data helpers

--- src/main/java/com/grubnest/game/core/databasehandler/utils/DataUtils.java
+++ src/main/java/com/grubnest/game/core/databasehandler/utils/DataUtils.java
@@ -1,5 +1,6 @@
 package com.grubnest.game.core.databasehandler.utils;
 
+import com.grubnest.game.core.databasehandler.DatabaseManager;
 import com.grubnest.game.core.databasehandler.MySQL;
 
 import java.sql.SQLException;
@@ -11,41 +12,38 @@ import java.util.UUID;
  * MySQL class
  *
  * @author Theeef
- * @version 1.0 at 6/2/2022
+ * @version 1.0.1 at 6/11/2022
  */
 public abstract class DataUtils {
 
     /**
      * Updates a players username stored in the database
      *
-     * @param mysql    the mysql instance
      * @param id       the player's uuid
      * @param username the player's username
      * @throws SQLException Thrown if database cannot be accessed
      */
-    public static void updatePlayerUsername(MySQL mysql, UUID id, String username) throws SQLException {
-        mysql.updatePlayerUsername(id, username);
+    public static void updatePlayerUsername(UUID id, String username) throws SQLException {
+        DatabaseManager.getInstance().getMySQL().updatePlayerUsername(id, username);
     }
 
     /**
      * Gets a player's UUID from their last stored username
      *
-     * @param mysql    the mysql instance
      * @param username the player's username
      * @return an optional containing the player's UUID, or nothing if an error occurred
      */
-    public static Optional<UUID> getIDFromUsername(MySQL mysql, String username) {
-        return mysql.getIdFromUsername(username);
+    public static Optional<UUID> getIDFromUsername(String username) {
+        return DatabaseManager.getInstance().getMySQL().getIdFromUsername(username);
     }
 
     /**
      * Get a players last stored username from their uuid
      *
-     * @param mysql the mysql instance
-     * @param id    the player's uuid
+     * @param id the player's uuid
      * @return the player's username
      */
-    public static Optional<String> getUsernameFromID(MySQL mysql, UUID id) {
-        return mysql.getUsernameFromID(id);
+    public static Optional<String> getUsernameFromID(UUID id) {
+        return DatabaseManager.getInstance().getMySQL().getUsernameFromID(id);
     }
 }
